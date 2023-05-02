1d87c581e657ea650989030a53ffc634a2dbb179
Mary <mcchristy@Marys-MacBook-Pro.local>
2023-05-02 12:51:07 -0400

    I have started my javascript code to being programming the application, I have created variables that pull elements from the html file

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-var quizContainer = document.getElementById('quiz-container')
+var quizContainer = document.getElementById("quiz-container");
+var question = document.getElementById("question");
+var answers = document.getElementById("answers");
+var timer = document.getElementById("timer");
+var startButton = document.getElementById("start");
+console.log(document.getElementById("start"));
